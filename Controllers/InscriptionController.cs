501de2acf41eed3fbc7571601aac6e4ab5b3c63e
Jose Edwards <jflira@miuandes.cl>
2023-03-19 15:47:47 -0300

    feat(inscription): add inscription controller with create and Index view

---
+++ Controllers/InscriptionController.cs
@@ -0,0 +1,25 @@
+﻿using RealEstateApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using RealEstateApp.Models;
+
+namespace RealEstateApp.Controllers
+{
+    public class InscriptionController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public InscriptionController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Inscription> objInscriptionList = _db.Inscriptions.ToList();
+            return View(objInscriptionList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
