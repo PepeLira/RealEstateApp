38132cafafa85b3f61450ac91d5d76f11ee885a7
Jose Edwards <jflira@miuandes.cl>
2023-03-20 22:09:43 -0300

    feat(MultiOwner): add a MultiOwner controller with index view

---
+++ Controllers/MultiOwnerController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstateApp.Data;
+using RealEstateApp.Models;
+
+namespace RealEstateApp.Controllers
+{
+    public class MultiOwnerController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public MultiOwnerController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<MultiOwner> objMultiOwnerList = _db.MultiOwners.ToList();
+            return View(objMultiOwnerList);
+        }
+    }
+}
