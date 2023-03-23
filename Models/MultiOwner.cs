be8b219bfe91081152fb2ca6503d2acc81cbc5d8
Jose Edwards <jflira@miuandes.cl>
2023-03-22 23:22:17 -0300

    style(MultiOwner): rename RoyaltyPercentage

--- Models/MultiOwner.cs
+++ Models/MultiOwner.cs
@@ -6,16 +6,30 @@ namespace RealEstateApp.Models
     {
         [Key]
         public int Id { get; set; }
-        public string Commune { get; set; } // Comuna
-        public int Block { get; set; } // Manzana 
-        public int Property { get; set; } // Predio
-        public string Owner { get; set; } // RUT
-        public int RoyaltyPer { get; set; } // % de derecho
+        public int Commune { get; set; }
+        public int Block { get; set; }
+        public int Property { get; set; }
+        public string Owner { get; set; }
+        public int RoyaltyPercentage { get; set; }
         public int Fojas { get; set; }
-        public DateTime InscriptionDate { get; set; } // Fecha de Inscripcion
-        public int InscriptionYear { get; set; } // Ano de Inscripcion
-        public int InscriptionNumber { get; set; } // N de Inscripcion
-        public int InitialEffectiveYear { get; set; } // Ano Vigencia Inicial
-        public int FinalEffectiveYear { get; set; } // Ano Vigencia Final
+        public DateTime InscriptionDate { get; set; }
+        public int InscriptionYear { get; set; }
+        public int InscriptionNumber { get; set; }
+        public int InitialEffectiveYear { get; set; }
+        public int FinalEffectiveYear { get; set; }
     }
 }
+
+/// Model Description
+/// 
+/// string    Commune               ==> Comuna
+/// int       Block                 ==> Manzana 
+/// int       Property              ==> Predio
+/// string    Owner                 ==> RUT
+/// int       RoyaltyPercentage     ==> % de derecho
+/// int       Fojas 
+/// DateTime  InscriptionDate       ==> Fecha de Inscripcion
+/// int       InscriptionYear       ==> Ano de Inscripcion
+/// int       InscriptionNumber     ==> N de Inscripcion
+/// int       InitialEffectiveYear  ==> Ano Vigencia Inicial
+/// int       FinalEffectiveYear    ==> Ano Vigencia Final
