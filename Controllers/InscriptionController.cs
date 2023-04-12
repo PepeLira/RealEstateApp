b12cd4b0588e50b1aaf037551faa92e17b7dbb41
rafapavez98 <repavez@miuandes.cl>
2023-04-12 11:48:30 -0400

    fix(InscriptionnController): inscription controller refactor

--- Controllers/InscriptionController.cs
+++ Controllers/InscriptionController.cs
@@ -10,33 +10,33 @@ namespace RealEstateApp.Controllers
 {
     public class InscriptionController : Controller
     {
-        private readonly ApplicationDbContext _db;
+        private readonly ApplicationDbContext dbContext;
 
-        public InscriptionController(ApplicationDbContext db)
+        public InscriptionController(ApplicationDbContext dbContext)
         {
-            _db = db;
+            this.dbContext = dbContext;
         }
 
         public IActionResult Index()
         {
-            IEnumerable<Inscription> objInscriptionList = _db.Inscriptions.ToList();
+            IEnumerable<Inscription> objInscriptionList = dbContext.Inscriptions.ToList();
             return View(objInscriptionList);
         }
         public IActionResult Create()
         {
             var communeOptions = Enum.GetNames(typeof(CommuneOptions));
-            var ComuneSelectList = new SelectList(communeOptions);
+            var comuneSelectList = new SelectList(communeOptions);
 
             var cneOptions = Enum.GetNames(typeof(CneOptions));
-            var CneSelectList = new SelectList(cneOptions);
+            var cneSelectList = new SelectList(cneOptions);
 
-            var buyersOptions = _db.Buyers.Select(item => new SelectListItem
+            var buyersOptions = dbContext.Buyers.Select(item => new SelectListItem
             {
                 Value = item.Id.ToString(),
                 Text = item.Rut
             }).ToList();
 
-            var sellersOptions = _db.Sellers.Select(item => new SelectListItem
+            var sellersOptions = dbContext.Sellers.Select(item => new SelectListItem
             {
                 Value = item.Id.ToString(),
                 Text = item.Rut
@@ -49,15 +49,15 @@ namespace RealEstateApp.Controllers
                 AvailableSellers = sellersOptions
             };
 
-            ViewBag.CommuneOptions = ComuneSelectList;
-            ViewBag.CneOptions = CneSelectList;
+            ViewBag.CommuneOptions = comuneSelectList;
+            ViewBag.CneOptions = cneSelectList;
 
             return View(viewModel);
         }
 
-        public IActionResult Details(int id)
+        public ViewResult Details(int id)
         {
-            Inscription inscription = _db.Inscriptions.Find(id);
+            Inscription inscription = dbContext.Inscriptions.Find(id);
             return View(inscription);
         }
 
@@ -67,15 +67,15 @@ namespace RealEstateApp.Controllers
         public IActionResult Create(CreateViewModel viewModel)
         {
             var inscription = viewModel.NewInscription;
-            Buyer? selected_buyer = _db.Buyers.Find(viewModel.SelectedBuyerId);
-            Seller? selected_seller = _db.Sellers.Find(viewModel.SelectedSellerId);
+            Buyer? selected_buyer = dbContext.Buyers.Find(viewModel.SelectedBuyerId);
+            Seller? selected_seller = dbContext.Sellers.Find(viewModel.SelectedSellerId);
 
             if (inscription != null && selected_buyer != null && selected_seller != null)
             {
                 selected_buyer.Inscriptions.Add(inscription);
                 selected_seller.Inscriptions.Add(inscription);
 
-                _db.SaveChanges();
+                dbContext.SaveChanges();
                 return RedirectToAction("Index");
             }
             return RedirectToAction("Create");
