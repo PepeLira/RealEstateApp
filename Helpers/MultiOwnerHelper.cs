be80ea53007cd7a084849eeb4243b0736031c6e5
Jose Edwards <jflira@miuandes.cl>
2023-04-18 23:00:15 -0400

    fix multiowners

--- Helpers/MultiOwnerHelper.cs
+++ Helpers/MultiOwnerHelper.cs
@@ -7,16 +7,22 @@ namespace RealEstateApp.Helpers
     public class MultiOwnerHelper
     {
         private readonly ApplicationDbContext _dbContext;
-        private readonly Buyer? _buyer;
+        private readonly IEnumerable<Buyer> _buyers;
+        private readonly Inscription? _newInscription;
 
         public MultiOwnerHelper(ApplicationDbContext dbContext)
         {
             _dbContext = dbContext;
-            _buyer = dbContext.Buyers.Where(e => e.Id > 0)
-            .OrderByDescending(e => e.Id)
+            _newInscription = dbContext.Inscriptions.Where(e => e.AttentionID > 0)
+            .OrderByDescending(e => e.AttentionID)
             .FirstOrDefault();
 
-            AddMultiOwners(_buyer);
+            _buyers = dbContext.Buyers.Where(b => b.Inscription.AttentionID == _newInscription.AttentionID);
+
+            foreach (var buyer in _buyers)
+            {
+                AddMultiOwners(buyer);
+            }
         }
 
         public void AddMultiOwners(Buyer buyer)
@@ -31,12 +37,37 @@ namespace RealEstateApp.Helpers
             newMultiOwner.InscriptionYear = buyer.Inscription.InscriptionDate.Year;
             newMultiOwner.InscriptionNumber = buyer.Inscription.InscriptionNumber;
             newMultiOwner.InscriptionDate = buyer.Inscription.InscriptionDate;
-            newMultiOwner.InitialEffectiveYear = buyer.Inscription.InscriptionDate.Year;
+            newMultiOwner.InitialEffectiveYear = ValidateInitialEffectiveYear(buyer.Inscription.InscriptionDate.Year);
             newMultiOwner.FinalEffectiveYear = null;
 
             _dbContext.Add(newMultiOwner);
 
+
             _dbContext.SaveChanges();
+        }
+
+        public int ValidateInitialEffectiveYear(int year)
+        {
+            if (year < 2019)
+            {
+                year = 2019;
+            }
+            return year;
+        }
+
+        public void ValidateActiveMultiOwnerYear(MultiOwner multiOwner)
+        {
+            var latestMultiOwner = _dbContext.MultiOwners
+            .OrderByDescending(mo => mo.InscriptionDate)
+            .FirstOrDefault();
+
+            
+
+        }
+
+        public bool IsMultiOwner1LaterThanMultiOwner2(MultiOwner multiOwner1, MultiOwner multiOwner2)
+        {
+            return multiOwner1.InscriptionDate > multiOwner2.InscriptionDate;
         }
     }
 }
